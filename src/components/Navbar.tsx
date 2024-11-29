1fd3d1cacd80602dc1759c3fc6f8b25d5880b47b
shadowDragons <shadow_dragon@126.com>
2024-11-29 10:54:19 +0800

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,14 @@
-"use client"
-import { cn } from "@/lib/utils";
+'use client'
+import { cn } from '@/lib/utils'
 
+import { Briefcase, FolderGit2, GraduationCap, HomeIcon, Mail, MoreHorizontal, User } from 'lucide-react'
 
-import {
-  Briefcase,
-  FolderGit2,
-  GraduationCap,
-  HomeIcon,
-  Mail,
-  MoreHorizontal,
- 
-  User,
-} from 'lucide-react';
+import { Dock, DockIcon, DockItem, DockLabel } from '@/components/animation/DockAnimation'
 
-import { Dock, DockIcon, DockItem, DockLabel } from '@/components/animation/DockAnimation';
-
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import FramerWrapper from "./animation/FramerWrapper";
-import { usePathname } from "next/navigation";
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import FramerWrapper from './animation/FramerWrapper'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
   // const items = [
@@ -33,74 +23,60 @@ const Navbar = () => {
   const data = [
     {
       title: 'Home',
-      icon: (
-        <HomeIcon className='h-full w-full ' />
-      ),
+      icon: <HomeIcon className='h-full w-full ' />,
       href: '/',
     },
-    {
-      title: 'About',
-      icon: (
-        <User className='h-full w-full ' />
-      ),
-      href: '/about',
-    },
+    // {
+    //   title: 'About',
+    //   icon: (
+    //     <User className='h-full w-full ' />
+    //   ),
+    //   href: '/about',
+    // },
     {
       title: 'Skills',
-      icon: (
-        <Briefcase className='h-full w-full ' />
-      ),
+      icon: <Briefcase className='h-full w-full ' />,
       href: '/skills',
     },
-    {
-      title: 'Education',
-      icon: (
-        <GraduationCap className='h-full w-full ' />
-      ),
-      href: '/education',
-    },
+    // {
+    //   title: 'Education',
+    //   icon: <GraduationCap className='h-full w-full ' />,
+    //   href: '/education',
+    // },
     {
       title: 'Projects',
-      icon: (
-        <FolderGit2 className='h-full w-full ' />
-      ),
+      icon: <FolderGit2 className='h-full w-full ' />,
       href: '/projects',
     },
-   
-    {
-      title: 'Contact us',
-      icon: (
-        <Mail className='h-full w-full ' />
-      ),
-      href: '/contact',
-    },
+    // {
+    //   title: 'Contact us',
+    //   icon: <Mail className='h-full w-full ' />,
+    //   href: '/contact',
+    // },
     {
       title: 'More',
-      icon: (
-        <MoreHorizontal className='h-full w-full ' />
-      ),
+      icon: <MoreHorizontal className='h-full w-full ' />,
       href: '/more',
     },
-  ];
-  const [scrolling, setScrolling] = useState(false);
+  ]
+  const [scrolling, setScrolling] = useState(false)
   const pathname = usePathname()
-  
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 0) {
-        setScrolling(true);
+        setScrolling(true)
       } else {
-        setScrolling(false);
+        setScrolling(false)
       }
-    };
+    }
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
     // <DockNavbar/>
@@ -110,7 +86,7 @@ const Navbar = () => {
     //       <TooltipProvider key={itm.name}>
     //         <Tooltip>
     //           <TooltipTrigger asChild>
-    //             <Link href={itm.link} 
+    //             <Link href={itm.link}
     //               className={cn(
     //                 buttonVariants({ variant: "ghost", size: "sm" }),"hover:text-[#2f7df4]",pathname === itm.link && "text-[#2f7df4] bg-zinc-100"
     //               )}
@@ -126,22 +102,21 @@ const Navbar = () => {
     //     );
     //   })}
     //   </FramerWrapper>
-    <div className={`fixed top-5 right-0 left-0 px-0 sm:px-5 m-auto w-full sm:w-fit bg-transparent z-[+9999999] ${scrolling ? "hidden":"block"}`}>
-    <Dock className='items-end pb-3 rounded-full'>
-      {data.map((item, idx) => (
-        <Link href={item.href} key={idx}>
-
-        <DockItem
-          className={cn("aspect-square rounded-full bg-gray-200 dark:bg-neutral-800",pathname === item.href && " bg-gray-100 border border-primary}")}
-          >
-          <DockLabel >{item.title}</DockLabel>
-          <DockIcon className={cn(pathname === item.href && "text-[#2f7df4]")}>{item.icon}</DockIcon>
-        </DockItem>
+    <div className={`fixed top-5 right-0 left-0 px-0 sm:px-5 m-auto w-full sm:w-fit bg-transparent z-[+9999999] ${scrolling ? 'hidden' : 'block'}`}>
+      <Dock className='items-end pb-3 rounded-full'>
+        {data.map((item, idx) => (
+          <Link href={item.href} key={idx}>
+            <DockItem
+              className={cn('aspect-square rounded-full bg-gray-200 dark:bg-neutral-800', pathname === item.href && ' bg-gray-100 border border-primary}')}
+            >
+              <DockLabel>{item.title}</DockLabel>
+              <DockIcon className={cn(pathname === item.href && 'text-[#2f7df4]')}>{item.icon}</DockIcon>
+            </DockItem>
           </Link>
-      ))}
-    </Dock>
+        ))}
+      </Dock>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
